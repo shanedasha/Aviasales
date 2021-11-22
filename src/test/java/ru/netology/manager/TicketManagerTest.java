a0e86ab3a226f08b8b91c84b75c5a77024aa6ca5
shanedasha <chirkova_99@inbox.ru>
2021-11-22 10:44:20 +0300

    Add tests

--- src/test/java/ru/netology/manager/TicketManagerTest.java
+++ src/test/java/ru/netology/manager/TicketManagerTest.java
@@ -9,7 +9,11 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 class TicketManagerTest {
     private Ticket first = new Ticket(1, 3000, "LED", "DME", 60);
     private Ticket second = new Ticket(2, 2000, "LED", "DME", 180);
-    private Ticket third = new Ticket(3, 3000, "KUF", "OGD", 40);
+    private Ticket third = new Ticket(3, 4000, "KUF", "OGD", 40);
+    private Ticket four = new Ticket(4, 5000, "LED", "DME", 40);
+    private Ticket five= new Ticket(5, 6000, "KUF", "DME", 40);
+    private Ticket six = new Ticket(6, 7000, "LED", "DME", 40);
+    private Ticket seven = new Ticket(7, 8000, "LED", "DME", 40);
     private TicketRepository repository = new TicketRepository();
     private TicketManager manager = new TicketManager(repository);
 
@@ -17,8 +21,11 @@ class TicketManagerTest {
     void shouldSortTickets() {
         manager.add(first);
         manager.add(second);
+        manager.add(seven);
+        manager.add(four);
+        manager.add(six);
 
-        Ticket[] expected = new Ticket[]{second, first};
+        Ticket[] expected = new Ticket[]{second, first, four,six, seven};
         Ticket[] actual = manager.FindAll("LED", "DME");
 
         assertArrayEquals(expected, actual);
@@ -31,6 +38,34 @@ class TicketManagerTest {
         manager.add(third);
 
         Ticket[] expected = new Ticket[]{second, first};
+        Ticket[] actual = manager.FindAll("LED", "DME");
+
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    void shouldFindTicketsZero() {
+
+        Ticket[] expected = new Ticket[]{};
+        Ticket[] actual = manager.FindAll("LED", "DME");
+
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    void shouldFindTicketsOne() {
+        manager.add(first);
+
+        Ticket[] expected = new Ticket[]{first};
+        Ticket[] actual = manager.FindAll("LED", "DME");
+
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    void shouldFindTickets() {
+        manager.add(first);
+        manager.add(four);
+        manager.add(six);
+
+        Ticket[] expected = new Ticket[]{first, four, six};
         Ticket[] actual = manager.FindAll("LED", "DME");
 
         assertArrayEquals(expected, actual);
